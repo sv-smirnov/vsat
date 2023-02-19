7c45da1c52f91749d0f3fa1259888b4b6492de47
Timur Guliev <mail@guliev-timur.ru>
2023-02-20 00:09:43 +0500

    add RestController

--- src/main/java/ru/rtrn/vsat/controllers/StationsController.java
+++ src/main/java/ru/rtrn/vsat/controllers/StationsController.java
@@ -1,15 +1,10 @@
 package ru.rtrn.vsat.controllers;
 
-import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ru.rtrn.vsat.entities.Station;
@@ -26,16 +21,6 @@ public class StationsController {
     public Map<String, Station> getAll() {
         
         return stationService.getMapStations();
-    }
-
-    @PutMapping("/sleep")
-    public String sleep(
-            @RequestBody String id
-    ) throws IOException, InterruptedException {
-        stationService.stopWashing(id);
-        System.out.println("Method update");
-        System.out.println(id);
-        return stationService.getMapStations().get(id).toString();
     }
     
 }

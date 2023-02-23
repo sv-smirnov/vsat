76cfd1da705318e24d1f36da99b2d43d8c8c8f22
Timur Guliev <mail@guliev-timur.ru>
2023-02-24 02:30:36 +0500

    button sleep PUT ready

--- src/main/java/ru/rtrn/vsat/controllers/StationsController.java
+++ src/main/java/ru/rtrn/vsat/controllers/StationsController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,15 +28,13 @@ public class StationsController {
         return stationService.getMapStations();
     }
 
-    @PutMapping(path="{id}")
+    @PutMapping("/sleep")
     public String sleep(
-            @PathVariable("id") String id,
-            @RequestBody String station
+            @RequestBody String id
     ) throws IOException, InterruptedException {
         stationService.stopWashing(id);
         System.out.println("Method update");
         System.out.println(id);
-        System.out.println(station);
         return stationService.getMapStations().get(id).toString();
     }
     

1e46c0e2da868a8df30cb283a75a0faa4a59fd3a
Timur Guliev <mail@guliev-timur.ru>
2023-02-24 02:52:29 +0500

    refactoring

--- src/main/java/ru/rtrn/vsat/controllers/StationsController.java
+++ src/main/java/ru/rtrn/vsat/controllers/StationsController.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +19,6 @@ public class StationsController {
     @Autowired
     private StationService stationService;
 
-    // @CrossOrigin
     @GetMapping("/list")
     public Map<String, Station> getAll() {
         
